47f2dd5c1e6205d4e7a3e02df541b3f6c5a54388
Charchit <charchit@amazon.com>
2017-08-11 22:40:20 +0530

    integrated EBS and front end

--- src/main/java/tr/com/lucidcode/config/HibernateConfiguration.java
+++ src/main/java/tr/com/lucidcode/config/HibernateConfiguration.java
@@ -1,5 +1,6 @@
 package tr.com.lucidcode.config;
 
+import java.io.IOException;
 import java.util.Properties;
 
 import org.hibernate.cfg.Environment;
@@ -11,9 +12,10 @@ public class HibernateConfiguration {
 	public static ServiceRegistry hibernateConfig() {
 		Properties properties = new Properties();
 		properties.setProperty(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
-		properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/finance");
+		properties.setProperty(Environment.URL, "jdbc:mysql://finance-db-identifier.cahy8ao5jcat.us-east-2.rds.amazonaws.com:3306/finance");
 		properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
-		properties.setProperty(Environment.USER, "root");
+		properties.setProperty(Environment.USER, System.getProperty("RDS_SECRET_USER"));
+		properties.setProperty(Environment.PASS, System.getProperty("RDS_SECRET_PASS"));
 
 		ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
 		serviceRegistryBuilder.applySettings(properties);
